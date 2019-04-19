17943c6d9d27785c608e804d03562d2003f25009
ZanGeng <78992317@qq.com>
2019-04-19 13:04:48 +0800

    vue-cli初始化完成，可以开始做项目了

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,4 +1,4 @@
-<template>
+ <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastClick from 'fastclick'
+import './assets/style/base.css'
+/* 避免移动端1px边框问题 */
+import './assets/style/border.css'
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
