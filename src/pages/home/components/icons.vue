f4a91e58bdab284e899bc4091f5dfd2c2c340411
ZanGeng <78992317@qq.com>
2019-04-20 16:38:17 +0800

    首页icons图标导航制作完成

--- src/pages/home/components/icons.vue
+++ src/pages/home/components/icons.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="icons">
-    <swiper :options="swiperOption"><!--使用了轮播vue-awesome-swiper插件，参考icons.vue-->
-      <!--第一页轮播-->
+    <swiper><!--使用了轮播vue-awesome-swiper插件，参考icons.vue-->
+      <!--方式1:（写死方式）-->
       <swiper-slide>
         <div class="icon">
           <div class="icon-img-div">
@@ -11,37 +11,37 @@
         </div>
         <div class="icon">
           <div class="icon-img-div">
-            <img class="icon-img-self" src="@/assets/images/q10.png" />
+            <img class="icon-img-self" src="@/assets/images/q11.png" />
           </div>
-          <p class="icon-p">城市景点2</p>
+          <p class="icon-p">城市景点1</p>
         </div>
         <div class="icon">
           <div class="icon-img-div">
             <img class="icon-img-self" src="@/assets/images/q10.png" />
           </div>
-          <p class="icon-p">城市景点3</p>
+          <p class="icon-p">城市景点1</p>
+        </div>
+        <div class="icon">
+          <div class="icon-img-div">
+            <img class="icon-img-self" src="@/assets/images/q11.png" />
+          </div>
+          <p class="icon-p">城市景点1</p>
         </div>
         <div class="icon">
           <div class="icon-img-div">
             <img class="icon-img-self" src="@/assets/images/q10.png" />
           </div>
-          <p class="icon-p">城市景点3</p>
+          <p class="icon-p">城市景点1</p>
         </div>
-        <div class="icon">
-          <div class="icon-img-div">
-            <img class="icon-img-self" src="@/assets/images/q10.png" />
-          </div>
-          <p class="icon-p">城市景点3</p>
-        </div>
-      </swiper-slide>
+       </swiper-slide>
 
-      <!--第二  页轮播-->
-      <swiper-slide>
-        <div class="icon">
+      <!--方式2：（读取数据库方式，并自动判断换页）-->
+      <swiper-slide v-for="(page,index) of pages" :key="index">
+        <div class="icon" v-for="item of page" :key="item.key">
           <div class="icon-img-div">
-            <img class="icon-img-self" src="@/assets/images/q10.png" />
+            <img class="icon-img-self" :src="item.imgUrl" />
           </div>
-          <p class="icon-p">城市景点</p>
+          <p class="icon-p">{{item.name}}</p>
         </div>
       </swiper-slide>
     </swiper>
@@ -50,7 +50,88 @@
 
 <script>
 export default {
-  name: 'HomeIcons'
+  name: 'HomeIcons',
+  data: function () {
+    return {
+      iconList: [{
+        imgUrl: require('@/assets/images/q10.png'),
+        key: '0001',
+        name: '北京城市'
+      }, {
+        imgUrl: require('@/assets/images/q11.png'),
+        key: '0002',
+        name: '山东城市'
+      }, {
+        imgUrl: require('@/assets/images/q10.png'),
+        key: '0003',
+        name: '云南城市'
+      }, {
+        imgUrl: require('@/assets/images/q11.png'),
+        key: '0004',
+        name: '云南城市'
+      }, {
+        imgUrl: require('@/assets/images/q10.png'),
+        key: '0005',
+        name: '云南城市'
+      }, {
+        imgUrl: require('@/assets/images/q11.png'),
+        key: '0006',
+        name: '云南城市'
+      }, {
+        imgUrl: require('@/assets/images/q10.png'),
+        key: '0007',
+        name: '云南城市'
+      }, {
+        imgUrl: require('@/assets/images/q11.png'),
+        key: '0008',
+        name: '云南城市'
+      }, {
+        imgUrl: require('@/assets/images/q10.png'),
+        key: '0009',
+        name: '云南城市'
+      }, {
+        imgUrl: require('@/assets/images/q11.png'),
+        key: '0010',
+        name: '云南城市'
+      }]
+    }
+  },
+  /* computed计算属性，有缓存效果，提升性能 */
+  computed: {
+    /* 自动计算需要循环的轮播页 ，并返回pages这个二维数组 */
+    pages: function () {
+      /* pages是一个二维数组，第一维度是<swiper-slide>，第二维度是<swiper-slide>里的 <div  */
+      var pages = []
+      /* 循环iconList，item这里是上面的对象 */
+      this.iconList.forEach(function (item, index) {
+        /* 取整，index=8时page为1，其余为0 */
+        var page = Math.floor(index / 8)
+        /* 如果pages[0]不存在就建立，如果pages[1]不存在就建立一个 */
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        /* 二维数组形成，page[0]=['1','2'],page[1]=['1','2']  */
+        pages[page].push(item)
+      })
+
+      return pages
+      /* 二维数组
+      * pages:Array[2]
+            0:Array[8]
+            0:Object
+            1:Object
+            2:Object
+            3:Object
+            4:Object
+            5:Object
+            6:Object
+            7:Object
+            1:Array[2]
+            0:Object
+            1:Object
+      * */
+    }
+  }
 }
 </script>
 
